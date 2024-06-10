2a4aa6e01ff47b51ff5cd7e3fab9ccaec12fd965
Jungki98 <150337599+Jungki98@users.noreply.github.com>
2024-06-10 17:51:05 +0900

    Update text.py

--- prototype/create/text.py
+++ prototype/create/text.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAI
 
-api_key = st.secrets["openai"]["api_key"]
+api_key = st.secrets["OPENAI_API_KEY"]
 
 class Output(BaseModel):
     result: list = Field(description="Comma separated key information")
@@ -43,3 +43,31 @@ def text_parser(input_text):
     result_list = [item.strip() for item in response.split(',')]
     parsed_result = Output(result=result_list)
     return parsed_result.result
+
+# category_AI
+def sub_title(input_text):
+    text_category_template = PromptTemplate.from_template(
+    """
+    You are the AI that chooses category. 
+    Please print out the word that most closely relates to the category list from below. 
+    You must only print out the spelling of that word.
+
+    category list: ["일시", "장소", "기타", "세부사항", "준비물", "비용", "유의사항", "관련 문의"]
+
+    If there is nothing appropriate in the category list, you can create and print your own top topics.
+
+    텍스트: {text}
+    """,
+    example = [{
+                "text": "6월 6일",
+                "result": "일시"
+            },
+            {
+                "text": "만해광장",
+                "result": "장소"
+            },]
+    )
+    llm = OpenAI(api_key=api_key,temperature=0)
+    result = llm(text_category_template.format(text=input_text))
+    subtext_category = result.strip()
+    return subtext_category
